0eb877877fc0ef135695cc1b3fe1e31c22c878f1
Moin Uddin <U_Moin75@yahoo.com>
2019-02-20 18:10:10 -0500

    Attempting recipe_batches

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,30 +3,15 @@
 import argparse
 
 def find_max_profit(prices):
-  maximum = max(prices)
-  minimum = min(prices)
-
-  remaining_max = 0
-
-  while prices.index(maximum) < prices.index(minimum):
-    prices.remove(maximum)
-    maximum = max(prices)
-    if prices.index(minimum) + 1 < len(prices):
-      prices.remove(minimum)
-      minimum = min(prices)
-    if len(prices) == 2: remaining_max = maximum
-
-  net = maximum - minimum
-  print(net)
-  if net == 0: return -(remaining_max - minimum)
-
-  return net
+  for i in range(0, len(prices)):
+    for j in range(i, len(prices)):
+      print(f'i: {prices[i]} j: {prices[j]} i-j: {prices[i] - prices[j]}')
 
 
-find_max_profit([10, 7, 5, 8, 11, 9])
-find_max_profit([100, 90, 80, 50, 20, 10])
+# find_max_profit([10, 7, 5, 8, 11, 9])
+# find_max_profit([100, 90, 80, 50, 20, 10])
 find_max_profit([1050, 270, 1540, 3800, 2])
-find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79])
+#find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79])
 
 
 if __name__ == '__main__':
