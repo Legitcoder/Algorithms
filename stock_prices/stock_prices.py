6b930bb786f6110498219d304296143b7f397914
Moin Uddin <U_Moin75@yahoo.com>
2019-02-20 13:56:57 -0500

    Still debugging stock_prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -11,6 +11,9 @@ def find_max_profit(prices):
   while prices.index(maximum) < prices.index(minimum):
     prices.remove(maximum)
     maximum = max(prices)
+    if prices.index(minimum) + 1 < len(prices):
+      prices.remove(minimum)
+      minimum = min(prices)
     if len(prices) == 2: remaining_max = maximum
 
   net = maximum - minimum
@@ -18,6 +21,12 @@ def find_max_profit(prices):
   if net == 0: return -(remaining_max - minimum)
 
   return net
+
+
+find_max_profit([10, 7, 5, 8, 11, 9])
+find_max_profit([100, 90, 80, 50, 20, 10])
+find_max_profit([1050, 270, 1540, 3800, 2])
+find_max_profit([100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79])
 
 
 if __name__ == '__main__':
