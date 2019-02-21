5a4038dbff227b5c5d8841cb3d0b57e361f17094
Moin Uddin <U_Moin75@yahoo.com>
2019-02-21 18:46:37 -0500

    Implement climbing_stairs minus cache

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,8 +3,19 @@
 import sys
 
 def climbing_stairs(n, cache=None):
-  pass 
+  if (n == 1 or n == 0):
+    return 1
+  elif (n == 2):
+    return 2
+  else:
+    cache = climbing_stairs(n - 3, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 1, cache)
+    return cache
 
+print(climbing_stairs(0))
+print(climbing_stairs(1))
+print(climbing_stairs(2))
+print(climbing_stairs(5))
+print(climbing_stairs(10))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
