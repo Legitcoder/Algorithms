cf10d79bddcc7e8207432730ee0b551449962c2c
Moin Uddin <U_Moin75@yahoo.com>
2019-02-21 20:22:40 -0500

    Cache Climbing Stairs

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -3,12 +3,20 @@
 import sys
 
 def climbing_stairs(n, cache=None):
+  if cache is None: cache = [0] * (n + 1)
+
+  if cache[n] > 0:
+    return cache[n]
+
   if (n == 1 or n == 0):
+    cache[n] = 1
     return 1
   elif (n == 2):
+    cache[n] = 2
     return 2
   else:
-    return climbing_stairs(n - 3, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 1, cache)
+    cache[n] = climbing_stairs(n - 3, cache) + climbing_stairs(n - 2, cache) + climbing_stairs(n - 1, cache)
+    return cache[n]
 
 print(climbing_stairs(0))
 print(climbing_stairs(1))
